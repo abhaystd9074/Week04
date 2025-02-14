9a2fa53888a2e51143d464697b3654d82d967259
Abhay Gautam <116936807+abhaystd9074@users.noreply.github.com>
2025-02-14 10:32:26 +0530

    Abhay Gautam add streams problem

---
+++ src/main/java/org/example/Problem1.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.*;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        String sourceFile = "source.txt";
+        String destinationFile = "destination.txt";
+
+        try (FileInputStream fis = new FileInputStream(sourceFile);
+             FileOutputStream fos = new FileOutputStream(destinationFile)) {
+
+            int byteData;
+            while ((byteData = fis.read()) != -1) {
+                fos.write(byteData);
+            }
+            System.out.println("File copied successfully!");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: Source file not found.");
+        } catch (IOException e) {
+            System.out.println("Error reading or writing file: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/example/Problem6.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.*;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        String sourceFile = "source.txt";
+        String destinationFile = "lowercase_output.txt";
+
+        convertUpperToLower(sourceFile, destinationFile);
+    }
+
+
+    private static void convertUpperToLower(String source, String destination) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(destination))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line.toLowerCase());
+                writer.newLine();
+            }
+
+            System.out.println("File converted successfully. Check " + destination);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: Source file not found.");
+        } catch (IOException e) {
+            System.out.println("Error reading or writing file: " + e.getMessage());
+        }
+    }
+}
